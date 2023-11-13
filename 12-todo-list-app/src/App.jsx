04e05ed357a1f75f202a9b3cf85b42cfb63286b7
tavber <tavber@tavber.idze>
2023-11-13 14:37:05 +0400

    davaleba

---
+++ 12-todo-list-app/src/App.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import TodoList from './components/TodoList'
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+      completedTodos: [],
+    };
+  }
+
+  handleAdd = () => {
+    const input = document.querySelector('.todo-list input');
+    const text = input.value.trim();
+
+    if (text === '') return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+    };
+
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, newTodo],
+    }));
+
+    input.value = '';
+  };
+
+  handleComplete = (id) => {
+    const { todos, completedTodos } = this.state;
+    const todoToComplete = todos.find((todo) => todo.id === id);
+
+    if (todoToComplete) {
+      this.setState((prevState) => ({
+        todos: prevState.todos.filter((todo) => todo.id !== id),
+        completedTodos: [...completedTodos, todoToComplete],
+      }));
+    }
+  };
+
+  handleDelete = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== id),
+      completedTodos: prevState.completedTodos.filter((todo) => todo.id !== id),
+    }));
+  };
+
+  handleBack = (id) => {
+    const { completedTodos, todos } = this.state;
+    const completedTodoToBack = completedTodos.find((todo) => todo.id === id);
+
+    if (completedTodoToBack) {
+      this.setState((prevState) => ({
+        completedTodos: prevState.completedTodos.filter((todo) => todo.id !== id),
+        todos: [...todos, completedTodoToBack],
+      }));
+    }
+  };
+
+  render() {
+    const { todos, completedTodos } = this.state;
+
+    return (
+      <div className="app">
+        <TodoList
+          todos={todos}
+          completedTodos={completedTodos}
+          onAdd={this.handleAdd}
+          onComplete={this.handleComplete}
+          onDelete={this.handleDelete}
+          onBack={this.handleBack}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+// ● დაწერე To do List აპლიკაცია, სადაც
+// გექნება ორი სვეტი, რომელშიც
+// მოთავსებული იქნება შესასრულებელი და
+// შესრულებული სამუშაოები
+// ● მომხმარებელს ექნება შესაძლებლობა
+// შეიყვანოს ახალი დავალებები, რომლებიც
+// ავტომატურად ჩავარდება შესასრულებელ
+// სამუშაოებში
+// ● შესასრულებელ სამუშაოებში ყოველ
+// დავალებას ექნება „დასრულება“ ღილაკი,
+// რომელზე დაჭერისას აღნიშნული
+// დავალება გადაინაცვლებს შესრულებული
+// დავალებების სიაში, ხოლო შესრულებული
+// დავალებების სიაში ყოველ დავალებას
+// ექნება წაშლის და შესასრულებელ
+// დავალებებში გადატანის ღილაკები
+
---
+++ 12-todo-list-app/src/components/Task.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+// import './Task.css';
+
+class Task extends Component {
+  handleComplete = () => {
+    this.props.onComplete(this.props.id);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.id);
+  };
+
+  handleBack = () => {
+    this.props.onBack(this.props.id);
+  };
+
+  render() {
+    const { text } = this.props;
+
+    return (
+      <div className="task">
+        <p>{text}</p>
+        <button onClick={this.handleComplete}>Complete</button>
+        <button onClick={this.handleDelete}>Delete</button>
+        <button onClick={this.handleBack}>Back</button>
+      </div>
+    );
+  }
+}
+
+export default Task;
